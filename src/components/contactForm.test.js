598c8510f8081f2cd48e0a3d5644302e1e080f8b
miriam-sullivan <maljayd42@gmail.com>
2020-04-09 17:32:26 -0500

    making progress

--- src/components/contactForm.test.js
+++ src/components/contactForm.test.js
@@ -4,8 +4,21 @@ import ContactForm from "./ContactForm";
 test("inputs are showing", () => {
   const { getByLabelText } = render(<ContactForm />);
 
-  getByLabelText(/firstName/i);
-  getByLabelText(/lastName/i);
+  getByLabelText(/first Name/i);
+  getByLabelText(/last Name/i);
   getByLabelText(/email/i);
   getByLabelText(/message/i);
+});
+
+test("form submits and prints out the info", () => {
+  const { getByLabelText, getByText, getByTestId } = render(<ContactForm />);
+
+  const FirstNameInput = getByPlaceholderText(/bill/i);
+  const LastNameInput = getByPlaceholderText(/luo/i);
+
+  fireEvent.change(FirstNameInput, { target: { value: "Mel" } });
+  fireEvent.change(LastNameInput, { target: { value: "Gibson" } });
+
+  expect(FirstNameInput.value).toBe("Mel");
+  expect(LastNameInput.value).toBe("Gibson");
 });
